ba9eac81b80449c55c23050b6171158b6026beb5
Dusan Jovanovic <dusandjovanovic@outlook.com>
2020-02-27 12:39:12 +0100

    Meta, API ui support and "dotnet run" configs changed

--- Microservices/src/Microservices.Api/Startup.cs
+++ Microservices/src/Microservices.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Microservices.Api
 {
@@ -29,6 +30,11 @@ namespace Microservices.Api
             services.AddRabbitMq(Configuration);
             services.AddScoped<IEventHandler<ActivityCreated>, ActivityCreatedHandler>();
             services.AddScoped<IActivityRepository, ActivityRepository>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Microservices API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +51,12 @@ namespace Microservices.Api
                 scope.ServiceProvider
                     .GetService<IDatabaseInitializer>().InitializeAsync();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Microservices API V1");
+            });
 
             app.UseAuthentication();
             app.UseMvc();
--- Microservices/src/Microservices.Services.Identity/Startup.cs
+++ Microservices/src/Microservices.Services.Identity/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Microservices.Services.Identity
 {
@@ -35,6 +36,11 @@ namespace Microservices.Services.Identity
             services.AddScoped<IEncrypter, Encrypter>();
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IUserService, UserService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Microservices.Identity API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +57,12 @@ namespace Microservices.Services.Identity
                 scope.ServiceProvider
                     .GetService<IDatabaseInitializer>().InitializeAsync();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Microservices.Identity API V1");
+            });
 
             app.UseMvc();
         }
